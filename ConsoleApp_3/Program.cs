d66e70271267c42e2716571dc01c60e2f5194c7e
Ruslan <myadres.ruslan@gmail.com>
2022-11-17 13:03:56 +0300

    Домашняя работа 5

--- ConsoleApp_3/Program.cs
+++ ConsoleApp_3/Program.cs
@@ -3,15 +3,18 @@
 Console.Write("Введите число: ");
 string? number = Console.ReadLine();
 
-void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
-    Console.WriteLine($"Ваше число: {number} - палиндром.");
-  }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
 }
 
-if (number!.Length == 5){
-  CheckingNumber(number);
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
 }
 else Console.WriteLine($"Введи правильное число");
 
@@ -31,15 +34,16 @@ int Metrica(string coorName, string pointName)
     return Convert.ToInt16(Console.ReadLine());
 }
 
-double Decision(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
+double Decision(double x1, double x2,
+                double y1, double y2,
+                double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) +
+                     Math.Pow((y2 - y1), 2) +
+                     Math.Pow((z2 - z1), 2));
 }
 
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2);
 
 Console.WriteLine($"Длина отрезка  {segmentLength}");
 
@@ -49,24 +53,28 @@ Console.WriteLine($"Длина отрезка  {segmentLength}");
 Console.Write("Введите число: ");
 int cube = Convert.ToInt32(Console.ReadLine());
 
-void Cube(int[] cube){
-  int counter = 0;
-  int length = cube.Length;
-  while (counter <  length){
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-    counter++;
-  }
+void Cube(int[] cube)
+{
+    int counter = 0;
+    int length = cube.Length;
+    while (counter < length)
+    {
+        cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
 }
 
-void PrintArry(int[] coll){
-  int count = coll.Length;
-  int index = 0;
-  while(index < count){
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
-} 
+void PrintArry(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(coll[index] + " ");
+        index++;
+    }
+}
 
-int[] arry = new int[cube+1];
+int[] arry = new int[cube + 1];
 Cube(arry);
 PrintArry(arry);
